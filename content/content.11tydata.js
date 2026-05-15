0fb33092e9183bff5befc670e1fd0a671bcac920
Drew Jaynes <xoodrew@gmail.com>
2026-05-15 11:18:59 -0700

    Change post permalinks to flat /{slug}/ to match WordPress URL structure

--- content/content.11tydata.js
+++ content/content.11tydata.js
@@ -1,8 +1,10 @@
 module.exports = {
   layout: "layouts/post.njk",
   eleventyComputed: {
-    permalink: data =>
-      data.page.filePathStem.replace(/^\/content\//, "/") + "/",
+    permalink: data => {
+      const parts = data.page.filePathStem.split("/");
+      return "/" + parts[parts.length - 1] + "/";
+    },
     category: data => {
       const parts = data.page.filePathStem.split("/");
       return parts.length >= 3 ? parts[parts.length - 2] : null;
